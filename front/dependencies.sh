e846108af9f3f02cb44aa6c8a8e1afa0cfe2123a
devarias <gedafu2005@gmail.com>
2021-05-02 03:03:31 -0500

    Added a package web-vitals int the script

--- front/dependencies.sh
+++ front/dependencies.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+npm init -y
 npm i -S antd
 npm i -S axios
 npm i -S react
@@ -6,3 +7,5 @@ npm i -S react-dom
 npm i -S react-highlight-words
 npm i -S react-router-dom
 npm i -S react-scripts
+npm i -S web-vitals
+sed -i '7i \\t\t"start": "react-scripts start",' package.json
